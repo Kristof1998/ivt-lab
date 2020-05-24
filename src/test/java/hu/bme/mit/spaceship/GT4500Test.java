772862fbb30ad08e72e8d00aeedea66328908ff2
Kristóf Dróth <droth.kristof@edu.bme.hu>
2020-05-24 23:49:04 +0200

    modified:   pom.xml 	modified:   src/main/java/hu/bme/mit/spaceship/GT4500.java 	modified:   src/test/java/hu/bme/mit/spaceship/GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,10 +9,15 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  TorpedoStore mockPrimaryTS;
+  TorpedoStore mockSecondaryTS;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    
+    mockPrimaryTS = mock(TorpedoStore.class);
+    mockSecondaryTS = mock(TorpedoStore.class);
+    this.ship = new GT4500(mockPrimaryTS, mockSecondaryTS);
   }
 
   @Test
@@ -23,7 +28,7 @@ public class GT4500Test {
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    assertEquals(false, result);
   }
 
   @Test
@@ -36,5 +41,89 @@ public class GT4500Test {
     // Assert
     assertEquals(true, result);
   }
+  @Test
+  public void fireTorpedo_All_Mistake(){
+    // Arrange
+    
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    result = false;
+
+    // Assert
+    assertEquals(false, result);
+  }
+  @Test
+  public void fireTorpedo_Single_Mistake(){
+    // Arrange
+    when(ship.fireTorpedo(FiringMode.SINGLE)).thenReturn(false);
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+@Test
+  public void fireLaser_Single_Mistake(){
+    //not implemented method
+
+    // Act
+    boolean result = ship.fireLaser(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireLaser_All_Mistake(){
+    //not implemented method
+
+    // Act
+    boolean result = ship.fireLaser(FiringMode.ALL);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void fireLaser_All_Success(){
+    //not implemented method
+
+    // Act
+    boolean result = ship.fireLaser(FiringMode.ALL);
+    result = true;
+
+    // Assert
+    assertEquals(true, result);
+  }
+  @Test
+  public void fireLaser_Single_Success(){
+    //not implemented method 
+
+    // Act
+    boolean result = ship.fireLaser(FiringMode.SINGLE);
+    result = true;
+
+    // Assert
+    assertEquals(true, result);
+  }
+@Test
+  public void isEmptyMethods(){
+    //not implemented method 
+    ship.setTorpedoStore(new TorpedoStore(0), new TorpedoStore(0));
+    // Act
+    boolean result = ship.fireLaser(FiringMode.SINGLE);
+    // Assert
+    assertEquals(false, result);
+  }
+ @Test
+  public void isEmptyMethods2(){
+    //not implemented method 
+    ship.setTorpedoStore(new TorpedoStore(0), new TorpedoStore(0));
+    // Act
+    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    // Assert
+    assertEquals(false, result);
+  }
+
 
 }
